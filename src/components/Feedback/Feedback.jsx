b2840ce338fea681541c1e86a79478dd117f2fe8
Daria Shpak <darya.shpak@gmail.com>
2023-06-29 23:47:27 +0300

    Update Feedback.jsx

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -8,26 +8,69 @@ import {
   Statistics,
   Item
 } from "./Feedback.styled"
-// import css from "styled-components"
 
-class FeedbackButtons extends React.Component {
+class Feedback extends React.Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  }
+
+  onLeaveFeedback = feedback => {
+    this.setState(prevState => ({
+      [feedback]: prevState[feedback] + 1
+    }))
+  }
+
+  countTotalFeedback = () => {
+    const {good, neutral, bad} = this.state
+    return good + neutral + bad
+  }
+
+  countPositiveFeedbackPercentage() {
+    const {good} = this.state
+    const totalFeedback = this.countTotalFeedback()
+    return totalFeedback > 0 ? Math.round(good / totalFeedback * 100) : 0
+  }
+
   render() {
+    const totalFeedback = this.countTotalFeedback()
+    const positiveFeedback = this.countPositiveFeedbackPercentage()
     return <Container>
         <Title>Please leave your feedback</Title>
 
         <BtnWrapper>
-          <Button type="button">Good</Button>
-          <Button type="button">Neutral</Button>
-          <Button type="button">Bad</Button>
+          <Button type="button" onClick={() => this.onLeaveFeedback("good")}>
+            Good
+          </Button>
+          <Button type="button" onClick={() => this.onLeaveFeedback("neutral")}>
+            Neutral
+          </Button>
+          <Button type="button" onClick={() => this.onLeaveFeedback("bad")}>
+            Bad
+          </Button>
         </BtnWrapper>
+
         <Subtitle>Statistics</Subtitle>
         <Statistics>
-          <Item>Good: 0</Item>
-          <Item>Neutral: 0</Item>
-          <Item>Bad: 0</Item>
+          <Item>
+            Good: {this.state.good}
+          </Item>
+          <Item>
+            Neutral: {this.state.neutral}
+          </Item>
+          <Item>
+            Bad: {this.state.bad}
+          </Item>
+          <Item>
+            Total: {totalFeedback}
+          </Item>
+          <Item>
+            Positive feedback: {positiveFeedback}%
+          </Item>
         </Statistics>
       </Container>
   }
 }
 
-export default FeedbackButtons
+export default Feedback
