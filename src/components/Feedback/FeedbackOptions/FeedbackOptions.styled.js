483a5a94b64a95d05c2e722e35740d40de046221
Daria Shpak <darya.shpak@gmail.com>
2023-07-01 01:20:47 +0300

    Update FeedbackOptions.styled.js

--- src/components/Feedback/FeedbackOptions/FeedbackOptions.styled.js
+++ src/components/Feedback/FeedbackOptions/FeedbackOptions.styled.js
@@ -1,30 +1,26 @@
-
 import styled from "styled-components"
 
 export const BtnWrapper = styled.div`
-margin-top: 15px;
-display: flex;
-gap: 15px`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 15px;
+  display: flex;
+  gap: 18px;
+`
 
 export const Button = styled.button`
   cursor: pointer;
   width: 65px;
-  background: #e7e9fc;
-  border-radius: 8px;
-  border: 2px solid #8e8f99;
-  color: #434455;
-
+  background: #eac161;
+  border-radius: 10px;
+  border: 2px solid #51748f;
+  color: #435055;
+  font-size: 15px;
   padding: 8px;
 
   &:hover {
-    border: 2px solid #2e2f42;
-    color: #2e2f42;
+    border: 2px solid #eac161;
+    background-color: #7694a1;
   }
-`
-export const Title = styled.h1`
-  margin-top: 0;
-  margin-bottom: 0;
-`
-
-export const Section = styled.div`
 `
