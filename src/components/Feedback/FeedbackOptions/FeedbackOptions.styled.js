c420686e3d8c0ea19869c99b80a80c8671981c80
Daria Shpak <darya.shpak@gmail.com>
2023-06-30 19:09:16 +0300

    Create FeedbackOptions.styled.js

---
+++ src/components/Feedback/FeedbackOptions/FeedbackOptions.styled.js
@@ -0,0 +1,30 @@
+
+import styled from "styled-components"
+
+export const BtnWrapper = styled.div`
+margin-top: 15px;
+display: flex;
+gap: 15px`
+
+export const Button = styled.button`
+  cursor: pointer;
+  width: 65px;
+  background: #e7e9fc;
+  border-radius: 8px;
+  border: 2px solid #8e8f99;
+  color: #434455;
+
+  padding: 8px;
+
+  &:hover {
+    border: 2px solid #2e2f42;
+    color: #2e2f42;
+  }
+`
+export const Title = styled.h1`
+  margin-top: 0;
+  margin-bottom: 0;
+`
+
+export const Section = styled.div`
+`
