e22f85e7276fb2b616a62ecb50c6adf87700e603
Daria Shpak <darya.shpak@gmail.com>
2023-07-01 01:20:57 +0300

    Update Feedback.styled.js

--- src/components/Feedback/Feedback.styled.js
+++ src/components/Feedback/Feedback.styled.js
@@ -3,9 +3,17 @@ import styled from "styled-components"
 
 export const Container = styled.div`
   display: flex;
+  justify-content: center;
+  align-items: center;
   flex-direction: column;
-  padding: 24px;
+  padding: 36px;
   gap: 24px;
-  color: #2e2f42;
+  margin-top: 15%;
+  margin-left: auto;
+  margin-right: auto;
+  color: #435055;
+  background-color: #c1d2d1;
+  border: 1px solid #51748f;
+  border-radius: 20px;
+  max-width: 320px;
 `
-
