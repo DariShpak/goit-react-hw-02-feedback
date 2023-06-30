295c5c6324305e7b3113d95f5a79cbbc01d6f38e
Daria Shpak <darya.shpak@gmail.com>
2023-07-01 01:20:52 +0300

    Update FeedbackOptions.jsx

--- src/components/Feedback/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/Feedback/FeedbackOptions/FeedbackOptions.jsx
@@ -1,26 +1,22 @@
 import React from "react"
 import PropTypes from "prop-types"
-import {Section, Title, Button, BtnWrapper} from "./FeedbackOptions.styled"
+import {Button, BtnWrapper} from "./FeedbackOptions.styled"
 
 const FeedbackOptions = ({options, onLeaveFeedback}) => {
   return (
-    <Section>
-      <Title>Please leave your feedback</Title>
-
-      <BtnWrapper>
-        {options.map(option => {
-          return (
-            <Button
-              key={option}
-              type="button"
-              onClick={() => onLeaveFeedback(option)}
-            >
-              {option}
-            </Button>
-          )
-        })}
-      </BtnWrapper>
-    </Section>
+    <BtnWrapper>
+      {options.map(option => {
+        return (
+          <Button
+            key={option}
+            type="button"
+            onClick={() => onLeaveFeedback(option)}
+          >
+            {option}
+          </Button>
+        )
+      })}
+    </BtnWrapper>
   )
 }
 FeedbackOptions.propTypes = {
