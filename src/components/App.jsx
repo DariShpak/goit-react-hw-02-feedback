05c10aa9a60cc11b52ca12d0f25cfe33625cc809
Daria Shpak <darya.shpak@gmail.com>
2023-06-30 19:07:48 +0300

    Makes App the root component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,59 @@
-import Feedback from "./Feedback/Feedback";
+import React from "react"
+import Statistics from "./Feedback/Statistics/Statistics"
+import FeedbackOptions from "./Feedback/FeedbackOptions/FeedbackOptions"
+import {Container} from "./Feedback/Feedback.styled"
 
 
+class App extends React.Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  }
 
-export const App = () => {
-  return (
-    <div>
-      <Feedback />
-    </div>
+  onLeaveFeedback = option => {
+    this.setState(prevState => ({
+      [option]: prevState[option] + 1
+    }))
+  }
 
+  countTotalFeedback = () => {
+    const {good, neutral, bad} = this.state
+    return good + neutral + bad
+  }
 
-  );
-};
+  countPositiveFeedbackPercentage() {
+    const {good} = this.state
+    const totalFeedback = this.countTotalFeedback()
+    return totalFeedback > 0 ? Math.round(good / totalFeedback * 100) : 0
+  }
+
+  render() {
+    const {good, neutral, bad} = this.state
+    const totalFeedback = this.countTotalFeedback()
+    const positiveFeedback = this.countPositiveFeedbackPercentage()
+
+    return (
+      <Container>
+        {
+          <FeedbackOptions
+            options={Object.keys(this.state)}
+            onLeaveFeedback={this.onLeaveFeedback}
+          />
+        }
+
+        {
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={totalFeedback}
+            positivePercentage={positiveFeedback}
+          />
+        }
+      </Container>
+    )
+  }
+}
+
+export default App
