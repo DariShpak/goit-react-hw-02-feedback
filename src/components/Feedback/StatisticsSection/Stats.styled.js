4b9150afc0dacc63f7aa602c2b0a430d25c7b662
Daria Shpak <darya.shpak@gmail.com>
2023-06-30 17:42:32 +0300

    Update Statistics section

---
+++ src/components/Feedback/StatisticsSection/Stats.styled.js
@@ -0,0 +1,16 @@
+import styled from "styled-components"
+
+export const StatsWrapper = styled.div`
+`
+export const Subtitle = styled.h2`
+  margin-top: 0;
+  margin-bottom: 0;
+`
+export const StatsList = styled.ul`list-style: none;
+         margin-top: 0;
+         margin-bottom: 0;
+         padding-left: 0;
+         display: flex;
+         flex-direction: column;
+         gap: 6px;`
+export const Item = styled.li``
